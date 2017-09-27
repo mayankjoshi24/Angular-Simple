ebf05890482fe7b246658ee2a5418c2a07c96012
mayankjoshi24 <mayankjoc.24@hotmail.com>
2017-09-27 18:44:07 +0530

    Incoming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { Data } from "./app";
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app';
+  name: string = '';
+  description: string = '';
+  status: string = '';
+
+  halo: Array<Data> = [];
+
+  showHide: false;
+  isValid: true;
+
+  isValidForm() {
+
+    return this.isValid;
+  }
+
+  showResult() {
+    this.halo.push(new Data(this.name, this.description, this.status))
+    
+    
+  }
+
+  onClose() {
+
+    this.halo.pop()
+
+
+  }
+
+  onEdit() {
+      
+      this.halo.pop()
+      this.halo.push(new Data(this.name, this.description, this.status))
+  }
+
+
 }
